2af64e15754f1f72e568ee77fad2a3db9b13b1f2
Eric Gallant <ericgallant@Erics-MacBook-Pro.local>
2018-03-14 19:13:10 -0300

    My First Commit

---
+++ src/main/java/ca/gallant/projects/App.java
@@ -0,0 +1,13 @@
+package ca.gallant.projects;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
